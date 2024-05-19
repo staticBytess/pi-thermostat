bb973a7f34121e49e46bb751d012d0f9d5d639ea
David Vigil <160567397+staticBytess@users.noreply.github.com>
2024-05-19 03:33:37 -0700

    now outputs data to a SPI display

--- thermostat.py
+++ thermostat.py
@@ -1,41 +1,73 @@
 import dht
-from machine import Pin
+from machine import Pin, SPI
 from time import sleep
+import ssd1306 #OLED display library
 
-def alternate():
-    blue.toggle()
-    green.toggle()
-    red.toggle()
-    sleep(0.2)
+
+# main function to measure room
+def thermostat():
+    sensor.measure()
     
-    blue.toggle()
-    green.toggle()
-    red.toggle()
-    sleep(0.2)
+    temp = (sensor.temperature() * 9/5) + 32
+    temp_str = "{:.1f}".format(temp)
+
+    humid = sensor.humidity()
+    humid_str = "{:.1f}".format(humid)
     
-def toggleOff():
-    blue.off()
-    red.off()
-    green.off()
+    displayData(temp_str, humid_str)
+
+#Displays temperature and humidty to OLED screen
+def displayData(temp, humid):
+    oled.fill(0)
+    
+    oled.text('Temperature: ', 5, 6, 1)
+    oled.text(temp, 5, 16, 1)
+    oled.text("Humidity: ", 5, 33, 1)
+    oled.text(humid + '%', 5, 43, 1)
+    
+    progressBar()
+    
+#Helper function to display the progress bar
+def progressBar():
+    symbolCount = 8
+    delay = .8
+    
+    for i in range(symbolCount):
+        oled.text('*', i*16+5, 58, 1)
+        oled.show()
+        sleep(delay)
 
 
-sensor = dht.DHT22(Pin(26))
-blue = machine.Pin(16, machine.Pin.OUT)
-red = machine.Pin(17, machine.Pin.OUT)
-green = machine.Pin(18, machine.Pin.OUT)
 
-toggleOff()
-sleep(2)
+# initialize sensor
+sensor = dht.DHT22(Pin(28))
 
-sensor.measure()
-temp = (sensor.temperature() * 9/5) + 32
+#GPIO declaration for pins on OLED
+spi_port = 1
+SCK = 10
+SDA = 11
+RES = 12
+DC = 14
+CS = 15
 
-humid = sensor.humidity()
+spi = SPI(
+    spi_port,
+    baudrate=1000000,
+    mosi=Pin(SDA),
+    sck=Pin(SCK))
 
-print(temp)
-alternate()
 
-if (temp < 80):
-    green.on()
-else:
-    red.on()
+# Initialize OLED display
+width = 128
+height = 64
+
+oled = ssd1306.SSD1306_SPI(width,height,
+    spi,
+    dc = Pin(DC),
+    res = Pin(RES),
+    cs = Pin(CS),
+    external_vcc = False
+    )
+
+while (1 == 1):
+    thermostat()
